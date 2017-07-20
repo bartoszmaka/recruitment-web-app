993d225a7fe9ff112bf49d4a2eb112aa1692fd8c
bartoszmaka <moncznybartosz@gmail.com>
2017-07-20 15:08:14 +0200

    Remove unused code

--- spec/controllers/regards_controller_spec.rb
+++ spec/controllers/regards_controller_spec.rb
@@ -1,9 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe RegardsController, type: :controller do
-  let(:user) { User.create(email: 'rails@rails.com', password: 'rails', age: 1, gender: 'male') }
-  let(:other_user) { User.create(email: 'rspec@rspec.com', password: 'rspec12', age: 1, gender: 'male') }
-
   describe 'post #create' do
     it 'redirects to users path' do
       post :create
