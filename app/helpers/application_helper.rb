f9ee4799db336f743e4b622e3969e83059f191bf
bartoszmaka <moncznybartosz@gmail.com>
2017-07-20 17:23:26 +0200

    Fix select fields default values

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def parsed_interest_types
+    Interest.interest_types.keys.map { |type| [type.humanize, type] }
+  end
+
+  def parsed_genders
+    User.genders.keys.map { |gender| [gender.humanize, gender] }
+  end
 end
